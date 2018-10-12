8fbac0d0bc7824e2c3e81d64d3328c74da9dd6b3
Elyse Burmester <burmesterelyse@gmail.com>
2018-10-12 14:07:30 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,17 @@
 def unsafe?(speed)
-
+if 40 > speed 
+  true 
+  elsif speed > 60
+  true 
+else 
+  false 
+end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed.between?(40,60) ? false : true
 end
 	
 
