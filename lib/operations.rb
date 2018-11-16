1fb9e1b1b0089c6b3424ce99df01b1abac91ad01
Nick Inman <nick@theoatmeal.com>
2018-11-16 00:39:07 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,22 @@
 def unsafe?(speed)
-
+if speed > 60
+  TRUE
+elsif speed < 40
+  TRUE
+else speed <= 60 && speed >= 40
+  FALSE
+  end
 end
 
 
 
 def not_safe?(speed)
+	result = (speed > 60 ? true : false);
+
+	result = (speed < 40 ? true : false);
 	
+	result = (speed >= 40 && speed <= 60 ? false : true);
+	return result
 end
 	
 
