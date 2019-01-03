9998d87ccbd310239031db47796121a8d054fa97
Bryn Bennett <bryn.bennett@gmail.com>
2019-01-03 21:24:36 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,17 @@
 def unsafe?(speed)
-
+  if speed > 60
+    return true
+  elsif speed < 40
+    return true
+  else
+    return false
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+	60 < speed || 40 > speed ?  true : false
 end
 	
 
