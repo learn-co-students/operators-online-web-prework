3d666e1b064ac4149ccd2e242889e09c36fba29a
boisterous-design-4553 <reemimhussein@outlook.com>
2019-01-17 11:52:38 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,20 @@
 def unsafe?(speed)
+  speed = int
+  
+  if speed <40 || >60
+    "safe"
+  else
+    "unsafe"
+  end
 
 end
 
 
 
 def not_safe?(speed)
+  speed = int
+  
+  speed <40 || >60 ? "safe":"unsafe"
 	
 end
 	
