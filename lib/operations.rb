5ee57e947b8632d76866d6565ab1efcbd0b2e5a0
astronomical-form-6016 <carltondgrandison@gmail.com>
2019-02-10 19:26:30 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,12 @@
 def unsafe?(speed)
-
+  if spead >= 60 && <= 40
+    return "unsafe"
+  else 
+    return "safe"
 end
 
-
-
 def not_safe?(speed)
-	
+	speed >= 60 && <= 40 ? "safe" : "unsafe"
 end
 	
 
