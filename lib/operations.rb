41999186fb8b7a57dc1c9df82ed6e1885ff8828e
alireza nadiri <alireza_nadiri70@yahoo.com>
2019-02-11 22:45:20 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,16 @@
-def unsafe?(speed)
 
+def unsafe?(speed)
+  if speed > 60 ? "unsafe" : "safe"
+  elsif speed < 40 ? "safe" : "unsafe"
+  else speed >= 40 ? "unsafe" : "safe"
 end
 
 
 
 def not_safe?(speed)
-	
+  if speed > 60 ? "not_safe" : "safe"
+  elsif speed < 40 ? "not_safe" : "safe"
+  else speed >= 50 ? "notsafe" : "safe"
 end
 	
 
