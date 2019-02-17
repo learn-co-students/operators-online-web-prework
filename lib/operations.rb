c5cd01bd605610602ae8076120a6d68d08cb15ec
lieby weintraub <liebahinda@gmail.com>
2019-02-17 21:59:02 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,16 @@
 def unsafe?(speed)
-if speed > 60 
-  true
-else speed < 40  
-  true 
-  end
 
-def not_safe?(speed)
-	speed > 60 && speed < 40 ? true : false
-end
+ if speed < 40 || speed > 60
+   true
+else
+		false
+  end
+end 
+
+
+ def not_safe?(speed)	
+		  speed < 40 || speed > 60 ? true : false
+end	
 	
 
 
