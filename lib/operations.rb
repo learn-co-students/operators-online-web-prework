9e1cb339b1eb25dfe9669f5fa9242d971479badc
delightful-tree-6762 <mduckworth@gmail.com>
2019-02-24 05:33:39 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,15 @@
 def unsafe?(speed)
+  speed = 79
+	if "speed > 60 && < 40"
+		"true"
+	else speed
+		"false"
 
 end
-
 
 
 def not_safe?(speed)
-	
+	speed = 79
+	speed < 40 && > 60 "true" : "false"
+
 end
-	
-
-
