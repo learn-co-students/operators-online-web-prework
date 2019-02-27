048c93f8d32f8ebaec60d290dd2e11b301db3c28
Kat B <katreesebarnes@gmail.com>
2019-02-27 21:01:25 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,17 +1,21 @@
 def unsafe?(speed)
   
-   speed ? > 60 : < 40
-  
-   return true 
+   if speed  > 60 
+   true 
+    elsif speed < 40
+    true
+  elsif speed  > 40 && speed < 60
+  false
  end
- 
 end
 
 
 
 def not_safe?(speed)
-	
+  speed  > 40 && speed < 60 ? false : true
+
 end
 	
+
 
 
