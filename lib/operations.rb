9a57bdbc7a3d387f9b8ab43b14235b9ffc81cf7f
Patrick A <akolo@hotmail.com>
2019-02-28 21:03:08 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,35 @@
 def unsafe?(speed)
+  
+#if speed is greater than 60
+  if Speed %  >  60 
+	returns "true"  (FAILED - 1)
+  end 
 
-end
+#if speed is less than 60
+  if Speed %  < 40
+    returns "true" 
+  end 
 
+  if Speed % >=40 or <= 60
+ 	Returns "false" 
+  end 
+end 
 
+Def not_safe?(speed)
 
-def not_safe?(speed)
+#if speed is greater than 60
+  
+Speed =  60 
 	
-end
-	
+Speed > 59 ? "true" : "False"
+ 
 
+#if speed is less than 60
+Speed = 40
+  
+Speed < 39 ? "true : "False"
 
+#if speed is between
+
+Speed >=40 or <= 60
+   	Returns "false" 
