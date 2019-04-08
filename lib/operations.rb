e7deee5af2bc369849ce4fc60e41594bf077873d
Kelley Chaplain <kmchaplain@gmail.com>
2019-04-08 04:14:17 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,15 @@
 def unsafe?(speed)
-
+  if speed < 40
+    true
+  elsif speed > 60
+    true
+  else speed
+    false
+  end
 end
 
-
-
 def not_safe?(speed)
-	
+	speed.between?(40,60) ? false : true
 end
 	
 
