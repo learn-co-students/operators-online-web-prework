8f3b1b28493f17baeb3a78e8804361030f2cfbe9
James Gomez <james.gomez320@yahoo.com>
2019-04-30 01:48:14 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,14 @@
 def unsafe?(speed)
-
+  speed > 60 ? TRUE : FALSE
+  speed < 40 ? TRUE : FALSE
+  SPEAD 40..60 ? FALSE: TRUE 
+  
 end
 
-
-
 def not_safe?(speed)
+  speed > 60 ? TRUE : FALSE
+  speed < 40 ? TRUE : FALSE
+  speed 40..60 ? FALSE : TRUE
 	
 end
 	
