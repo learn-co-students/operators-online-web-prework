7918b1e0796a488c6c71ea77750e5df52122342d
Annie Truex <artruex@gmail.com>
2019-05-10 04:32:19 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,16 @@
 def unsafe?(speed)
-
+  if speed > 60
+    "true"
+  else speed < 40
+    "true"
+  elsif speed
+    "false"
 end
 
 
 
 def not_safe?(speed)
-	
+	speed > 60 : "true" : "fasle"
 end
 	
 
