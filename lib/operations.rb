a9a52f839153df6316e68e0bf616a1538231d36f
Huy do <huyddo40@gmail.com>
2019-05-14 17:49:31 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,15 @@
 def unsafe?(speed)
-
+  speed <= 40 || speed >= 60 ? true : false
+  # if speed <= 40 || speed >= 60
+  #     return true
+  # else 
+  #   return false
 end
 
 
 
 def not_safe?(speed)
-	
+	speed <= 40 || speed >= 60 ? true : false
 end
 	
 
