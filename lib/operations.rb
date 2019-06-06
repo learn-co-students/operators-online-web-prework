bd40ec8287291e4aa6027b0a1f5bcaf0c6f47bc0
Aubrey R <a.renault2019@outlook.com>
2019-06-06 01:53:14 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,35 +1,3 @@
-def unsafe?(speed)
-
-  if speed > 60
-      "unsafe"
-  else
-      "safe"
-    
-end
-
-def unsafe?(speed)
-    
-    if speed < 40
-        "unsafe"
-        
-    else "safe"    
-
 def not_safe?(speed)
-	
-	speed < 40 ? "unsafe" : "safe" 
+  speed < 40 || speed > 60 ? true : false
 end
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
